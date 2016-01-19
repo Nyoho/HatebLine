90a9a46cae8c2a9efca60df46bc17bd23df6bd0b
北䑓如法 <algebraicallyClosedField@gmail.com>
2016-01-19 16:25:36 +0900

    Rewrite the profileImage property in User for async

--- Models/User.swift
+++ Models/User.swift
@@ -14,13 +14,23 @@ import Cocoa
 class User: NSManagedObject {
 
 // Insert code here to add functionality to your managed object subclass
-    var profileImage: NSImage?  {
-        if let n = name {
+    var __profileImage: NSImage? = nil
+    
+    var profileImage: NSImage? {
+        if let image = self.__profileImage {
+            return image
+        } else {
+            if let n = name {
             let twoLetters = (n as NSString).substringToIndex(2)
-            if let url = NSURL(string: "http://cdn1.www.st-hatena.com/users/\(twoLetters)/\(n)/profile.gif"), let data = NSData(contentsOfURL: url) {
-            return NSImage(data: data)
+            if let url = NSURL(string: "http://cdn1.www.st-hatena.com/users/\(twoLetters)/\(n)/profile.gif") {
+                Alamofire.request(.GET, url).response { request, response, data, error in
+                    if let d = data {
+                        self.__profileImage = NSImage(data: d)
+                    }
+                }
+                }
             }
+            return nil
         }
-        return nil
     }
 }
