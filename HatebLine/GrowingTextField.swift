cfe25065b7bd5489b4644b90658754bea3bac211
北䑓如法 <algebraicallyClosedField@gmail.com>
2016-01-21 00:53:59 +0900

    Add GrowingTextField

---
+++ HatebLine/GrowingTextField.swift
@@ -0,0 +1,19 @@
+//
+//  GrowingTextField.swift
+//  HatebLine
+//
+//  Created by 北䑓 如法 on 16/1/20.
+//  Copyright © 2016年 北䑓 如法. All rights reserved.
+//
+
+import Cocoa
+
+class GrowingTextField: NSTextField {
+
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
