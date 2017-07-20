2461fcd8dd05644295c7a4f01357ea856dccc5c1
北䑓如法 <algebraicallyClosedField@gmail.com>
2017-07-20 23:10:09 +0900

    Update the Page model

--- Models/Page+CoreDataProperties.swift
+++ Models/Page+CoreDataProperties.swift
@@ -2,22 +2,41 @@
 //  Page+CoreDataProperties.swift
 //  HatebLine
 //
-//  Created by 北䑓 如法 on 16/1/22.
-//  Copyright © 2016年 北䑓 如法. All rights reserved.
-//
-//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
-//  to delete and recreate this implementation file for your updated model.
+//  Created by 北䑓 如法 on 17/7/19.
+//  Copyright © 2017年 北䑓 如法. All rights reserved.
 //
 
 import Foundation
 import CoreData
 
+
 extension Page {
 
-    @NSManaged var content: String?
-    @NSManaged var count: NSNumber?
-    @NSManaged var title: String?
-    @NSManaged var url: String?
-    @NSManaged var bookmarks: NSSet?
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Page> {
+        return NSFetchRequest<Page>(entityName: "Page")
+    }
+
+    @NSManaged public var content: String?
+    @NSManaged public var count: NSNumber?
+    @NSManaged public var title: String?
+    @NSManaged public var url: String?
+    @NSManaged public var bookmarks: NSSet?
+
+}
+
+// MARK: Generated accessors for bookmarks
+extension Page {
+
+    @objc(addBookmarksObject:)
+    @NSManaged public func addToBookmarks(_ value: Bookmark)
+
+    @objc(removeBookmarksObject:)
+    @NSManaged public func removeFromBookmarks(_ value: Bookmark)
+
+    @objc(addBookmarks:)
+    @NSManaged public func addToBookmarks(_ values: NSSet)
+
+    @objc(removeBookmarks:)
+    @NSManaged public func removeFromBookmarks(_ values: NSSet)
 
 }
