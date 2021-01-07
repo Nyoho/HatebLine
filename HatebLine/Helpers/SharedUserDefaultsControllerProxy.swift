f718d164a8c6fbc007e815281ee0c090044fe983
北䑓如法 <algebraicallyClosedField@gmail.com>
2021-01-07 18:00:24 +0900

    Update to https in comments

--- HatebLine/Helpers/SharedUserDefaultsControllerProxy.swift
+++ HatebLine/Helpers/SharedUserDefaultsControllerProxy.swift
@@ -5,7 +5,7 @@
 //  Created by 北䑓 如法 on 16/2/11.
 //  Copyright © 2016年 北䑓 如法. All rights reserved.
 //
-//  Thx: http://stackoverflow.com/questions/29312106/xcode-6-os-x-storyboard-multiple-user-defaults-controllers-bug-with-multiple-sce
+//  Thx: https://stackoverflow.com/questions/29312106/xcode-6-os-x-storyboard-multiple-user-defaults-controllers-bug-with-multiple-sce
 
 import Cocoa
 import Foundation
