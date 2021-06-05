89f2bbfeb7cc4ccf5f6e10d8dc31b66d090b0953
北䑓如法 <algebraicallyClosedField@gmail.com>
2021-06-05 17:57:21 +0900

    Add button sign-in sign-out

--- HatebLine/GeneralPrefViewController.swift
+++ HatebLine/GeneralPrefViewController.swift
@@ -7,10 +7,22 @@
 //
 
 import Cocoa
+import Question
 
 class GeneralPrefViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+    }
+
+    @IBAction func signin(_: Any) {
+        guard !QuestionBookmarkManager.shared.authorized else { return }
+        let vc = QuestionAuthViewController.loadFromNib()
+        presentAsModalWindow(vc)
+        QuestionBookmarkManager.shared.authenticate(viewController: vc)
+    }
+
+    @IBAction func signOut(_: Any) {
+        QuestionBookmarkManager.shared.signOut()
     }
 }
