c45b9bb4f002717fec18faa14acf860b48fae305
北䑓如法 <algebraicallyClosedField@gmail.com>
2023-01-03 11:45:44 +0900

    Use NSPersistentContainer
    macOS 10.12で導入された NSPersistentContainer を使った書き方に変更してみる。

--- HatebLine/Bootstrap/AppDelegate.swift
+++ HatebLine/Bootstrap/AppDelegate.swift
@@ -17,144 +17,97 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
     // MARK: -
 
     func applicationDidFinishLaunching(_: Notification) {
-        UserDefaults.standard.register(defaults: ["enableNotification": true])
+        UserDefaults.standard.register(defaults: ["jp.nyoho.HatebLine": true])
     }
 
     func applicationWillTerminate(_: Notification) {
         // Insert code here to tear down your application
     }
 
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
     // MARK: - Core Data stack
 
-    lazy var applicationDocumentsDirectory: URL = {
-        // The directory the application uses to store the Core Data store file. This code uses a directory named "jp.nyoho.HatebLine" in the user's Application Support directory.
-        let urls = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask)
-        let appSupportURL = urls[urls.count - 1]
-        return appSupportURL.appendingPathComponent("jp.nyoho.HatebLine")
-    }()
-
-    lazy var managedObjectModel: NSManagedObjectModel = {
-        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
-        let modelURL = Bundle.main.url(forResource: "HatebLine", withExtension: "momd")!
-        return NSManagedObjectModel(contentsOf: modelURL)!
-    }()
-
-    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+    lazy var persistentContainer: NSPersistentContainer = {
         /*
-         The persistent store coordinator for the application.
-         This implementation creates and returns a coordinator, having added the store for the application to it.
-         (The directory for the store is created, if necessary.)
-         This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
-         */
-        let fileManager = FileManager.default
-        var failError: NSError?
-        var shouldFail = false
-        var failureReason = "There was an error creating or loading the application's saved data."
-
-        // Make sure the application files directory is there
-        do {
-            let properties = try (self.applicationDocumentsDirectory as NSURL).resourceValues(forKeys: [URLResourceKey.isDirectoryKey])
-            if !(properties[URLResourceKey.isDirectoryKey]! as AnyObject).boolValue {
-                failureReason = "Expected a folder to store application data, found a file \(self.applicationDocumentsDirectory.path)."
-                shouldFail = true
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+        */
+        let container = NSPersistentContainer(name: "HatebLine")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                 
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error)")
             }
-        } catch {
-            let nserror = error as NSError
-            if nserror.code == NSFileReadNoSuchFileError {
-                do {
-                    try fileManager.createDirectory(atPath: self.applicationDocumentsDirectory.path, withIntermediateDirectories: true, attributes: nil)
-                } catch {
-                    failError = nserror
-                }
-            } else {
-                failError = nserror
-            }
-        }
-
-        // Create the coordinator and store
-        var coordinator: NSPersistentStoreCoordinator?
-        if failError == nil {
-            coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-            let url = self.applicationDocumentsDirectory.appendingPathComponent("CocoaAppCD.storedata")
-            do {
-                try coordinator!.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: url, options: nil)
-            } catch {
-                failError = error as NSError
-            }
-        }
-
-        if shouldFail || (failError != nil) {
-            // Report any error we got.
-            var dict = [String: AnyObject]()
-            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data" as AnyObject?
-            dict[NSLocalizedFailureReasonErrorKey] = failureReason as AnyObject?
-            if failError != nil {
-                dict[NSUnderlyingErrorKey] = failError
-            }
-            let error = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
-            NSApplication.shared.presentError(error)
-            abort()
-        } else {
-            return coordinator!
-        }
-    }()
-
-    lazy var managedObjectContext: NSManagedObjectContext = {
-        /* Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.)
-         This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
-         */
-        let coordinator = self.persistentStoreCoordinator
-        var managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
-        managedObjectContext.persistentStoreCoordinator = coordinator
-        return managedObjectContext
+        })
+        return container
     }()
 
     // MARK: - Core Data Saving and Undo support
 
-    @IBAction func saveAction(_: AnyObject!) {
+    @IBAction func saveAction(_ sender: AnyObject?) {
         // Performs the save action for the application, which is to send the save: message to the application's managed object context. Any encountered errors are presented to the user.
-        if !managedObjectContext.commitEditing() {
+        let context = persistentContainer.viewContext
+
+        if !context.commitEditing() {
             NSLog("\(NSStringFromClass(type(of: self))) unable to commit editing before saving")
         }
-        if managedObjectContext.hasChanges {
+        if context.hasChanges {
             do {
-                try managedObjectContext.save()
+                try context.save()
             } catch {
+                // Customize this code block to include application-specific recovery steps.
                 let nserror = error as NSError
                 NSApplication.shared.presentError(nserror)
             }
         }
     }
 
-    func windowWillReturnUndoManager(_: NSWindow) -> UndoManager? {
+    func windowWillReturnUndoManager(window: NSWindow) -> UndoManager? {
         // Returns the NSUndoManager for the application. In this case, the manager returned is that of the managed object context for the application.
-        return managedObjectContext.undoManager
+        return persistentContainer.viewContext.undoManager
     }
 
     func applicationShouldTerminate(_ sender: NSApplication) -> NSApplication.TerminateReply {
         // Save changes in the application's managed object context before the application terminates.
-
-        if !managedObjectContext.commitEditing() {
+        let context = persistentContainer.viewContext
+        
+        if !context.commitEditing() {
             NSLog("\(NSStringFromClass(type(of: self))) unable to commit editing to terminate")
             return .terminateCancel
         }
-
-        if !managedObjectContext.hasChanges {
+        
+        if !context.hasChanges {
             return .terminateNow
         }
-
+        
         do {
-            try managedObjectContext.save()
+            try context.save()
         } catch {
             let nserror = error as NSError
+
             // Customize this code block to include application-specific recovery steps.
             let result = sender.presentError(nserror)
-            if result {
+            if (result) {
                 return .terminateCancel
             }
-
+            
             let question = NSLocalizedString("Could not save changes while quitting. Quit anyway?", comment: "Quit without saves error question message")
-            let info = NSLocalizedString("Quitting now will lose any changes you have made since the last successful save", comment: "Quit without saves error question info")
+            let info = NSLocalizedString("Quitting now will lose any changes you have made since the last successful save", comment: "Quit without saves error question info");
             let quitButton = NSLocalizedString("Quit anyway", comment: "Quit anyway button title")
             let cancelButton = NSLocalizedString("Cancel", comment: "Cancel button title")
             let alert = NSAlert()
@@ -162,13 +115,14 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
             alert.informativeText = info
             alert.addButton(withTitle: quitButton)
             alert.addButton(withTitle: cancelButton)
-
+            
             let answer = alert.runModal()
-            if answer == NSApplication.ModalResponse.alertFirstButtonReturn {
+            if answer == .alertSecondButtonReturn {
                 return .terminateCancel
             }
         }
         // If we got here, it is time to quit.
         return .terminateNow
     }
+
 }
