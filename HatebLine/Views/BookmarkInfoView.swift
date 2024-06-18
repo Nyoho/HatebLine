d5e65532bd81d250206299f7a9fbf48903ba8571
北䑓如法 <algebraicallyClosedField@gmail.com>
2024-06-18 20:38:20 +0900

    Make the shape of tag a capsule in info panel

--- HatebLine/Views/BookmarkInfoView.swift
+++ HatebLine/Views/BookmarkInfoView.swift
@@ -175,8 +175,7 @@ struct TagsView: View {
                         .font(.caption)
                         .padding(.horizontal, 8)
                         .padding(.vertical, 4)
-                        .background(Color.accentColor.opacity(0.1))
-                        .cornerRadius(4)
+                        .background(Capsule().fill(Color.accentColor.opacity(0.1)))
                 }
             }
         }
