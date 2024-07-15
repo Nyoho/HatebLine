9e389d7ce78443c823d1ea5e97c51906b1acf18a
北䑓如法 <algebraicallyClosedField@gmail.com>
2024-07-16 00:12:01 +0900

    [preference] Set sign-in and sign-out buttons enabling/disabling

--- HatebLine/GeneralPrefViewController.swift
+++ HatebLine/GeneralPrefViewController.swift
@@ -11,26 +11,43 @@ import Question
 
 class GeneralPrefViewController: NSViewController {
     @IBOutlet var usernameLabel: NSTextField!
+    @IBOutlet var signInButton: NSButton!
+    @IBOutlet var signOutButton: NSButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
-        setUsername()
+        updateUI()
     }
 
     @IBAction func signin(_: Any) {
         guard !QuestionBookmarkManager.shared.authorized else { return }
         let vc = QuestionAuthViewController.loadFromNib()
-        presentAsModalWindow(vc)
+        presentAsSheet(vc)
         QuestionBookmarkManager.shared.authenticate(viewController: vc)
     }
 
     @IBAction func signOut(_: Any) {
         QuestionBookmarkManager.shared.signOut()
+        updateUI()
     }
 
-    func setUsername() {
-        if QuestionBookmarkManager.shared.authorized, let name = QuestionBookmarkManager.shared.displayName {
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        updateUI()
+    }
+
+    override func dismiss(_ viewController: NSViewController) {
+        super.dismiss(viewController)
+        updateUI()
+    }
+
+    private func updateUI() {
+        let isAuthorized = QuestionBookmarkManager.shared.authorized
+
+        signInButton?.isEnabled = !isAuthorized
+        signOutButton?.isEnabled = isAuthorized
+
+        if isAuthorized, let name = QuestionBookmarkManager.shared.displayName {
             usernameLabel.stringValue = name
         } else {
             usernameLabel.stringValue = "(Not signed in yet)"
